a10f966a43b2be84a544797e44f92214520b4b69
Tdvr <s1224624@student.windesheim.nl>
2026-03-08 21:52:33 +0100

    Feat: voeg kcal, hoeveelheid setter en plantaardig alternatief toe aan Ingredient

--- ingredient.py
+++ ingredient.py
@@ -1,9 +1,27 @@
 class Ingredient:
-    def __init__(self, naam: str, hoeveelheid: float, eenheid: str=""):
+    def __init__(self, naam, hoeveelheid, eenheid="", kcal=0):
         self.naam = naam
         self.hoeveelheid = hoeveelheid
         self.eenheid = eenheid
+        self.kcal = kcal
+        self.plantaardig_alternatief = None
 
-    def __str__(self):        
-       return f"{self.naam} - {self.hoeveelheid} {self.eenheid}"
-   
+    def set_hoeveelheid(self, hoeveelheid):
+        self.hoeveelheid = hoeveelheid
+
+    def get_hoeveelheid(self):
+        return self.hoeveelheid
+
+    def get_kcal(self):
+        return self.kcal
+
+    def set_plantaardig_alternatief(self, alternatief):
+        self.plantaardig_alternatief = alternatief
+
+    def get_ingredient(self, plantaardig):
+        if plantaardig and self.plantaardig_alternatief is not None:
+            return self.plantaardig_alternatief
+        return self
+
+    def __str__(self):
+        return f"{self.hoeveelheid} {self.eenheid} {self.naam}"
