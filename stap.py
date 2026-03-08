a172237c74d7ad45462f5b58d1aba649822a2780
Tdvr <s1224624@student.windesheim.nl>
2026-03-08 22:31:50 +0100

    feat: voeg optionele tip toe aan Stap

--- stap.py
+++ stap.py
@@ -1,6 +1,7 @@
 class Stap:
-    def __init__(self, beschrijving: str):
+    def __init__(self, beschrijving, tip=None):
         self.beschrijving = beschrijving
+        self.tip = tip
 
     def __str__(self):
         return self.beschrijving
