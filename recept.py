d6af2e1c181c9c84c788d8959a63b8b22a247048
Tdvr <s1224624@student.windesheim.nl>
2026-03-08 22:33:13 +0100

    feat: voeg aantal_personen, kcal totaal en plantaardig ondersteuning toe aan Recept

--- recept.py
+++ recept.py
@@ -3,11 +3,15 @@ from stap import Stap
 
 
 class Recept:
-    def __init__(self, naam: str, omschrijving: str):
+    def __init__(self, naam, omschrijving):
         self.naam = naam
         self.omschrijving = omschrijving
         self.ingredienten = []
         self.stappen = []
+        self.aantal_personen = 1
+
+    def get_naam(self):
+        return self.naam
 
     def voeg_ingredient_toe(self, ingredient):
         self.ingredienten.append(ingredient)
@@ -15,16 +19,39 @@ class Recept:
     def voeg_stap_toe(self, stap):
         self.stappen.append(stap)
 
-    def toon_details(self):
+    def get_ingredienten(self):
+        return self.ingredienten
+
+    def set_aantal_personen(self, personen):
+        for ingredient in self.ingredienten:
+            nieuwe_hoeveelheid = float(ingredient.get_hoeveelheid()) * personen
+            ingredient.set_hoeveelheid(str(nieuwe_hoeveelheid))
+        self.aantal_personen = personen
+
+    def get_aantal_personen(self):
+        return self.aantal_personen
+
+    def get_plantaardig_recept(self, plantaardig):
+        totaal_kcal = 0
         print("Naam: " + self.naam)
         print("Omschrijving: " + self.omschrijving)
+        print("Aantal personen: " + str(self.aantal_personen))
 
         print("\nIngredienten:")
         for ingredient in self.ingredienten:
-            print("  - " + ingredient.hoeveelheid + " " + ingredient.eenheid + " " + ingredient.naam)
+            gekozen = ingredient.get_ingredient(plantaardig)
+            print("  - " + str(gekozen))
+            totaal_kcal += gekozen.get_kcal() * self.aantal_personen
 
         print("\nStappen:")
         nummer = 1
         for stap in self.stappen:
             print("  " + str(nummer) + ". " + stap.beschrijving)
+            if stap.tip is not None:
+                print("     Tip: " + stap.tip)
             nummer += 1
+
+        print("\nTotaal kcal: " + str(totaal_kcal) + " kcal")
+
+    def toon_details(self):
+        self.get_plantaardig_recept(False)
